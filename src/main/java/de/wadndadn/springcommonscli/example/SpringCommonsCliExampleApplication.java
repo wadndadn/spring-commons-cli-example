e398f323b7bf8e7f89c522ad1d29e4b5b1b0c2e5
Christian Schubert <544196+SchubertCh@users.noreply.github.com>
2023-01-17 11:28:25 +0100

    Initial commit

---
+++ src/main/java/de/wadndadn/springcommonscli/example/SpringCommonsCliExampleApplication.java
@@ -0,0 +1,13 @@
+package de.wadndadn.springcommonscli.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCommonsCliExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCommonsCliExampleApplication.class, args);
+    }
+
+}
